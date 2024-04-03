a85ae4b2355eda87295155ad2f768485fe38707c
sathish s <s.sathishmechddesign@gmail.com>
2024-04-03 15:32:50 +0530

    chore: favicon changed

--- src/components/SideNavBar.tsx
+++ src/components/SideNavBar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { MenuItem, navBarLinks } from "../../constants";
-import {DownOutlined} from '@ant-design/icons'
+import { DownOutlined } from "@ant-design/icons";
 
 export function SideNavbar() {
   return (
@@ -9,13 +9,17 @@ export function SideNavbar() {
         to="/"
         className="text-2xl font-semibold flex items-center gap-2 p-6 mb-6"
       >
-        <img src="/hexagon.svg" className="invert" alt="hexagon-icon" />
+        <img src="/hexagon.svg" alt="hexagon-icon" />
         Dashboard
       </Link>
       <Menu items={navBarLinks} />
       <div className="mt-auto w-full pb-6">
         <div className="flex items-center gap-3 p-3 bg-slate-50/5 backdrop-blur-lg border border-slate-400/20">
-          <img src="/images/user.webp" alt="Evano" className="size-10 object-cover rounded-full" />
+          <img
+            src="/images/user.webp"
+            alt="Evano"
+            className="size-10 object-cover rounded-full"
+          />
           <div className="flex-1">
             <p className="text-sm">Evano</p>
             <p className="text-xs">Project Manager</p>
